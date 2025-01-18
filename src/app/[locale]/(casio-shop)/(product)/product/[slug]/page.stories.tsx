ecae3c1783a76be958834e8fa1fdcc8a057e3421
Alexey Lukin <llamerr@gmail.com>
2025-01-18 12:39:19 +0200

    chore: fix some sotries

--- src/app/[locale]/(casio-shop)/(product)/product/[slug]/page.stories.tsx
+++ src/app/[locale]/(casio-shop)/(product)/product/[slug]/page.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
-  title: 'Pages/HomePage',
+  title: 'Pages/ProductPage',
   component: () => null,
   argTypes: {
   },
@@ -9,10 +9,9 @@ const meta = {
     docs: {
       description: {
         component:
-        'Homepage is the main page of the website. It include these components:<br/>'
-        + '- [HeroBanner](?path=/docs/blocks-herobanner--docs)<br/>'
-        + '- [TrustBlock](?path=/docs/blocks-trustblock--docs)<br/>'
-        + '- [GenderBlock](?path=/docs/blocks-genderblock--docs)<br/>'
+        'Product page show details about single product. It include these components:<br/>'
+        + '- [ProductBanner](?path=/docs/blocks-productbanner--docs)<br/>'
+        + '- [Breadcrumbs](?path=/docs/blocks-breadcrumbs--docs)<br/>'
         + '- [ProductCollections](?path=/docs/blocks-productcollections--docs)<br/>'
         + '- [NewArrivals](?path=/docs/blocks-newarrivals--docs)<br/>'
         + '- [SolarBlock](?path=/docs/blocks-solarblock--docs)<br/>'
@@ -25,6 +24,6 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const HomePageDefault = {
+export const ProductPageDefault = {
   args: {},
 } satisfies Story;
--- src/blocks/HeroProduct/HeroProduct.stories.tsx
+++ src/blocks/HeroProduct/HeroProduct.stories.tsx
@@ -3,9 +3,17 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { HeroProduct } from './HeroProduct';
 
 const meta = {
-  title: 'Blocks/Product/HeroProduct',
+  title: 'Blocks/HeroProduct',
   component: HeroProduct,
   argTypes: {
+    image: {
+      type: 'string',
+      control: { type: 'text' },
+    },
+    title: {
+      type: 'string',
+      control: { type: 'text' },
+    },
   },
   parameters: {
     docs: {
@@ -20,5 +28,8 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const HeroProductDefault = {
-  args: {},
+  args: {
+    image: 'https://utfs.io/f/oM0zaDGq1OeYuVvZ45OwsazxZgkH7CmI6byMJqi5WTXrdcl1',
+    title: 'Casio G-Shock GA-100',
+  },
 } satisfies Story;
