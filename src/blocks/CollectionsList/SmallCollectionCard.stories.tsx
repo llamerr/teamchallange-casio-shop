3d1c3e5812df1f371e5127559e5cc7797750f358
Alexey Lukin <llamerr@gmail.com>
2025-02-16 21:27:07 +0200

    chore: update SmallCollectionCard, remove unused fields

--- src/blocks/CollectionsList/SmallCollectionCard.stories.tsx
+++ src/blocks/CollectionsList/SmallCollectionCard.stories.tsx
@@ -12,10 +12,6 @@ const meta = {
       type: 'string',
       control: { type: 'text' },
     },
-    description: {
-      type: 'string',
-      control: { type: 'text' },
-    },
     href: {
       type: 'string',
       control: { type: 'text' },
@@ -40,8 +36,6 @@ type Story = StoryObj<typeof meta>;
 export const SmallCollectionCardWithImage = {
   args: {
     title: 'G-Shock',
-    slug: 'g-shock',
-    description: 'Toughness meets bold style',
     href: '/collections/g-shock',
     image: PORTRAIT_IMAGES[0],
   },
@@ -50,8 +44,6 @@ export const SmallCollectionCardWithImage = {
 export const SmallCollectionCardWithoutImage = {
   args: {
     title: 'G-Shock',
-    slug: 'g-shock',
-    description: 'Toughness meets bold style',
     href: '/collections/g-shock',
   },
 } satisfies Story;
@@ -59,9 +51,6 @@ export const SmallCollectionCardWithoutImage = {
 export const SmallCollectionCardLongTitleAndDescription = {
   args: {
     title: 'G-Shock: The Ultimate in Rugged Timekeeping Technology and Bold Design',
-    slug: 'g-shock-the-ultimate-in-rugged-timekeeping-technology-and-bold-design',
-    description:
-      'Toughness meets bold style. The G-Shock collection is for those who refuse to back down from a challenge. With its rugged design and advanced technology, it is the perfect timepiece for those who push themselves to the limit.',
     href: '/collections/g-shock',
     image: PORTRAIT_IMAGES[1],
   },
